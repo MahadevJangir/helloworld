65d402d1ff192b71b1319cf682d5a16bb29563d2
ganesh palnitkar <ganesh@autofact.com>
2017-04-07 08:57:23 +0000

    file updated

--- src/main/java/com/autofact/helloworldapp/App.java
+++ src/main/java/com/autofact/helloworldapp/App.java
@@ -1,14 +1,9 @@
 package com.autofact.helloworldapp;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World! welcome to the training session on DevOps, Today is Sunday" );
-	System.out.println( "Hello World! welcome to the training session on DevOps, Today is Saturday" );
     }
 }
